288e99c6e9d3ec3cb4c1eeefc5fb42e23819d0b8
Brian Cooper <brian@brian-cooper.com>
2023-08-03 00:29:33 -0500

    feature(examples): add Panda to Next.js example

---
+++ examples/next/panda.config.ts
@@ -0,0 +1,17 @@
+import { anirefPreset } from "@animareflection/ui";
+import { defineConfig } from "@pandacss/dev";
+
+/**
+ * Panda configuration.
+ *
+ * @see https://panda-css.com/docs/references/config
+ */
+const pandaConfig = defineConfig({
+  preflight: true,
+  emitPackage: true,
+  presets: ["@pandacss/dev/presets", anirefPreset],
+  include: ["./src/**/*.{ts,tsx}"],
+  outdir: "src/generated/panda",
+});
+
+export default pandaConfig;
