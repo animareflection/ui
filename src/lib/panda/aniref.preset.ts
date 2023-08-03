2a3332d54fc291e40106da7916d7052b4a1303e7
Brian Cooper <brian@brian-cooper.com>
2023-08-03 01:06:17 -0500

    chore: move comment placement

--- src/lib/panda/aniref.preset.ts
+++ src/lib/panda/aniref.preset.ts
@@ -1,9 +1,9 @@
 import { definePreset } from "@pandacss/dev";
 
-// TODO remove explicit type annotation, required due to `pnpm` bug (and therefore Yarn with `pnpm` linker); https://github.com/microsoft/TypeScript/issues/47663
 /**
  * Anima Reflection Panda CSS preset.
  */
+// TODO remove explicit type annotation, required due to `pnpm` bug (and therefore Yarn with `pnpm` linker); https://github.com/microsoft/TypeScript/issues/47663
 const anirefPreset: ReturnType<typeof definePreset> = definePreset({
   theme: {
     extend: {
