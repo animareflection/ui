82db4c909b0f9d1078bdf7bc477f52ead2b504ed
Brian Cooper <brian@brian-cooper.com>
2023-08-02 22:01:03 -0500

    feature: inject package CSS, export Panda CSS and JSX constructs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+// inject root cascade layers
+import "lib/styles/main.css";
+
+// 🐼 export backfill of Panda components
+export * from "generated/panda/css";
+export * from "generated/panda/jsx";
+
+// export Panda preset (to be used in downstream Panda configurations)
+export { default as anirefPreset } from "lib/panda/aniref.preset";
+
+// export components
 export * from "./components";
