bcc23cd7651f9d8a0ee12a8df72541c63a22c9ab
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-08-07 20:27:48 -0500

    chore(utils): add appropriate JSDoc to 'getChildrenOnDisplayName'

--- src/lib/utils/getChildrenOnDisplayName.ts
+++ src/lib/utils/getChildrenOnDisplayName.ts
@@ -7,6 +7,10 @@ interface Props {
   displayName: string;
 }
 
+/**
+ * Verify if the children of a component have specific display names.
+ * This is useful for components that have subcomponents, like a `Card` component that has a `CardHeader` component as a child.
+ */
 const getChildrenOnDisplayName = ({ children, displayName }: Props) =>
   Children.map(children, (child) =>
     child.type.displayName === displayName ? child : null,
