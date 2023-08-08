b67cf30008826ec68ca558edde99ca1a145da159
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-08-08 00:18:54 -0500

    chore(imports): update component import patterns

--- src/components/client/core/Button/Button.stories.tsx
+++ src/components/client/core/Button/Button.stories.tsx
@@ -1,4 +1,4 @@
-import Button from "./Button";
+import { Button } from "components/client";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
--- src/components/client/core/Button/Button.test.tsx
+++ src/components/client/core/Button/Button.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it } from "@jest/globals";
 import { render } from "@testing-library/react";
 
-import Button from "./Button";
+import { Button } from "components/client";
 
 describe("Button", () => {
   // eslint-disable-next-line jest/expect-expect
