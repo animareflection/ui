3e72a1e073b6c2ad07f5ec8f894d11f9b134b8e4
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-08-13 17:04:08 -0500

    Button Variants (#40)
    * chore(button): move button recipe into specified recipe file
    
    * chore(button): update button recipe, adjust button component accordingly
    
    * feat(button): update stories to render each individual variant
    
    * chore(button): remove unnecessary font weight from button variants
    
    * refactor(button): update order of variants in recipe
    
    * refactor(button): update default padding of button component
    
    * chore(button): allow for custom className to still be passed in button component
    
    * feat(button): add storybook controls for button component
    
    * chore(button): remove unnecessary variant prop in default args, remove unnecessary comment about render function for button story
    
    * ci(changesets): add changeset for Button component variants
    
    * chore(button): update imports

---
+++ src/components/client/core/Button/Button.recipe.ts
@@ -0,0 +1,55 @@
+import { cva } from "generated/panda/css";
+
+export const recipe = cva({
+  base: {
+    cursor: "pointer",
+    color: "white",
+    bgColor: "brand.primary.500",
+    fontWeight: "bold",
+    px: 3,
+    py: 2,
+    borderRadius: "md",
+    _hover: { bgColor: "brand.primary.200" },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+  variants: {
+    variant: {
+      primary: {
+        color: "white",
+        bgColor: "brand.primary.500",
+      },
+      secondary: {
+        color: "brand.primary.500",
+        bgColor: "transparent",
+        border: "1px solid",
+        borderColor: "brand.primary.500",
+        _hover: {
+          bgColor: "transparent",
+          borderColor: "brand.primary.200",
+          color: "brand.primary.200",
+        },
+      },
+      ghost: {
+        bgColor: "transparent",
+        color: "brand.primary.500",
+        _hover: {
+          bgColor: "transparent",
+          color: "brand.primary.200",
+        },
+        _active: {
+          bgColor: "transparent",
+          color: "brand.primary.200",
+        },
+        _focus: {
+          bgColor: "transparent",
+          color: "brand.primary.200",
+        },
+      },
+      round: {
+        borderRadius: "full !important",
+      },
+    },
+  },
+});
--- src/components/client/core/Button/Button.tsx
+++ src/components/client/core/Button/Button.tsx
@@ -1,35 +1,27 @@
 import { Pressable } from "@ark-ui/react";
 
-import { cva } from "generated/panda/css";
+import { recipe as button } from "./Button.recipe";
+import { cx } from "generated/panda/css";
 import { panda } from "generated/panda/jsx";
 
 import type { PandaComponent } from "generated/panda/types/jsx";
 import type { ComponentProps } from "react";
 
-const recipe = cva({
-  base: {
-    cursor: "pointer",
-    color: "white",
-    bgColor: "brand.primary.500",
-    fontWeight: "bold",
-    p: 3,
-    borderRadius: "md",
-    _hover: { bg: "brand.primary.200" },
-  },
-  variants: {
-    size: {
-      sm: { fontSize: "sm" },
-      lg: { fontSize: "lg" },
-    },
-  },
-});
+export interface Props extends ComponentProps<typeof PandaButton> {
+  variant?: "primary" | "secondary" | "ghost" | "round";
+}
 
-export type Props = ComponentProps<typeof Button>;
+const PandaButton: PandaComponent<typeof Pressable> = panda(Pressable, button);
 
 /**
  * Core UI button.
  */
-// TODO remove explicit type annotation, required due to `pnpm` bug (and therefore Yarn with `pnpm` linker); https://github.com/microsoft/TypeScript/issues/47663
-const Button: PandaComponent<typeof Pressable> = panda(Pressable, recipe);
+const Button = ({ variant, children, className, ...rest }: Props) => {
+  return (
+    <PandaButton className={cx(button({ variant }), className)} {...rest}>
+      {children}
+    </PandaButton>
+  );
+};
 
 export default Button;
