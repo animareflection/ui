35c313a18094891fe10eafe5027d718820f164af
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-08-13 14:25:20 -0500

    chore(button): remove unnecessary variant prop in default args, remove unnecessary comment about render function for button story

--- src/components/client/core/Button/Button.stories.tsx
+++ src/components/client/core/Button/Button.stories.tsx
@@ -5,7 +5,6 @@ import type { Meta, StoryObj } from "@storybook/react";
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  // NB: `render` instead of `args` is a workaround here, since Panda can't detect the need to recompile if story args are set. This has the drawback of less interactivity in corresponding stories (e.g. lack of an interactive Storybook colorpicker for the `color` prop). See https://github.com/chakra-ui/panda/issues/1057
   args: {
     children: "Click me 🏝️",
   },
