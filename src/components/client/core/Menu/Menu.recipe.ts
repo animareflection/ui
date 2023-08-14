734969048ee7969e380d28513e06197944f4183e
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-08-13 19:09:19 -0500

    feat(menu): add base for custom menu slot recipe

---
+++ src/components/client/core/Menu/Menu.recipe.ts
@@ -0,0 +1,159 @@
+import { sva } from "generated/panda/css";
+
+const itemStyle = {
+  alignItems: "center",
+  borderRadius: "xs",
+  cursor: "pointer",
+  display: "flex",
+  fontWeight: "medium",
+  textStyle: "sm",
+  transitionDuration: "fast",
+  transitionProperty: "background, color",
+  transitionTimingFunction: "default",
+  _hover: {
+    background: "#f5f5f5",
+  },
+  _highlighted: {
+    background: "#f5f5f5",
+  },
+} as const;
+
+export const recipe = sva({
+  slots: [
+    "root",
+    "arrow",
+    "arrowTip",
+    "content",
+    "contextTrigger",
+    "item",
+    "itemGroup",
+    "itemGroupLabel",
+    "optionItem",
+    "positioner",
+    "separator",
+    "trigger",
+    "triggerItem",
+  ],
+  base: {
+    itemGroupLabel: {
+      fontWeight: "semibold",
+      textStyle: "sm",
+    },
+    content: {
+      background: "white",
+      borderRadius: "sm",
+      borderWidth: "1px",
+      boxShadow: "lg",
+      _hidden: {
+        display: "none",
+      },
+      display: "flex",
+      flexDirection: "column",
+      outline: "none",
+    },
+    itemGroup: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    item: itemStyle,
+    optionItem: itemStyle,
+    triggerItem: itemStyle,
+  },
+  defaultVariants: {
+    size: "md",
+  },
+  variants: {
+    size: {
+      xs: {
+        itemGroup: {
+          gap: 1,
+        },
+        itemGroupLabel: {
+          py: 1.5,
+          px: 1.5,
+          mx: 1,
+        },
+        content: {
+          py: 1,
+          gap: 1,
+          minW: 24,
+        },
+        item: {
+          h: 8,
+          px: 1.5,
+          mx: 1,
+        },
+        optionItem: {
+          h: 8,
+          px: 1.5,
+          mx: 1,
+        },
+        triggerItem: {
+          h: 8,
+          px: 1.5,
+          mx: 1.5,
+        },
+      },
+      sm: {
+        itemGroup: {
+          gap: 1,
+        },
+        itemGroupLabel: {
+          py: 2,
+          px: 2,
+          mx: 1,
+        },
+        content: {
+          py: 1,
+          gap: 1,
+          minW: 32,
+        },
+        item: {
+          h: 9,
+          px: 2,
+          mx: 1,
+        },
+        optionItem: {
+          h: 9,
+          px: 2,
+          mx: 1,
+        },
+        triggerItem: {
+          h: 9,
+          px: 2,
+          mx: 1.5,
+        },
+      },
+      md: {
+        itemGroup: {
+          gap: 1,
+        },
+        itemGroupLabel: {
+          py: 2.5,
+          px: 2.5,
+          mx: 1,
+        },
+        content: {
+          py: 1,
+          gap: 1,
+          minW: 44,
+        },
+        item: {
+          h: 10,
+          px: 2.5,
+          mx: 1,
+        },
+        optionItem: {
+          h: 10,
+          px: 2.5,
+          mx: 1,
+        },
+        triggerItem: {
+          h: 10,
+          px: 2.5,
+          mx: 1.5,
+        },
+      },
+    },
+  },
+});
