8c6dea8654ae9920b48ea8894352d22bed9b78cb
Brian Cooper <brian@brian-cooper.com>
2023-08-14 22:54:53 -0500

    feature(theme): add fonts and global CSS

---
+++ src/lib/panda/fonts.ts
@@ -0,0 +1,13 @@
+/**
+ * Theme font tokens.
+ */
+const fonts = {
+  primary: {
+    value: `var(--font-primary), "Helvetica Neue", Helvetica, Arial, sans-serif`,
+  },
+  code: {
+    value: `var(--font-code), monospace`,
+  },
+} as const;
+
+export default fonts;
---
+++ src/lib/panda/globalCss.ts
@@ -0,0 +1,20 @@
+import { defineGlobalStyles } from "@pandacss/dev";
+
+// NB: relative path needed for Storybook
+import fonts from "./fonts";
+
+import type { GlobalStyleObject } from "generated/panda/types";
+
+// TODO remove explicit type annotation, required due to `pnpm` bug (and therefore Yarn with `pnpm` linker); https://github.com/microsoft/TypeScript/issues/47663
+const globalCss: GlobalStyleObject = defineGlobalStyles({
+  ":root": {
+    // alias fonts to CSS variables
+    "--font-primary": `"Ageo", sans-serif`,
+    "--font-code": `"JetBrains Mono", monospace`,
+  },
+  "html,body": {
+    fontFamily: fonts.primary.value,
+  },
+});
+
+export default globalCss;
