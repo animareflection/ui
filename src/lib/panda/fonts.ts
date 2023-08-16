76dca1fbab6a5b390926c81885e47d0ba7a5083a
Brian Cooper <brian@brian-cooper.com>
2023-08-15 21:44:40 -0500

    feature(types): annotate fonts configuration with type, add font token descriptions

--- src/lib/panda/fonts.ts
+++ src/lib/panda/fonts.ts
@@ -1,12 +1,16 @@
+import type { ThemeKey } from "lib/types";
+
 /**
  * Theme font tokens.
  */
-const fonts = {
+const fonts: ThemeKey<"fonts"> = {
   primary: {
     value: `var(--font-primary), "Helvetica Neue", Helvetica, Arial, sans-serif`,
+    description: "Primary font",
   },
   code: {
     value: `var(--font-code), monospace`,
+    description: "Code font",
   },
 } as const;
 
