79f47fd7fafdfcd92b4e0a99364d0e1414bdfc1f
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2023-08-16 21:06:34 -0500

    feature: add accordion.test

---
+++ src/components/client/core/Accordion/Accordion.test.tsx
@@ -0,0 +1,35 @@
+import { describe, it } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import { FiChevronDown as ChevronDown } from "react-icons/fi";
+
+import { Accordion, Icon } from "components/client";
+import { Text } from "components/universal";
+
+const items = ["panel-1", "panel-2", "panel-3"];
+
+const MockAccordion = () => (
+  <>
+    {items.map((item, id) => (
+      <Accordion icon={AccordionIcon} key={id} value={item} trigger={item}>
+        <Text>{item} content</Text>
+      </Accordion>
+    ))}
+  </>
+);
+
+describe("Drawer", () => {
+  it("renders accordion correctly", () => {
+    render(<MockAccordion />);
+
+    expect(screen.getByRole("button", { name: "panel-1" })).toBeInTheDocument();
+  });
+});
+
+const AccordionIcon = (props: { isOpen: boolean }) => {
+  const iconStyles = {
+    transform: props.isOpen ? "rotate(-180deg)" : undefined,
+    transition: "transform 0.4s",
+    transformOrigin: "center",
+  };
+  return <Icon style={iconStyles} as={ChevronDown} color="white" />;
+};
