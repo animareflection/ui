74d474f50c1a2dd10bbf859cce2034feba4eb31c
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-08-22 17:00:27 -0500

    chore(image): move image component into universal module

--- src/components/client/core/index.ts
+++ src/components/client/core/index.ts
@@ -2,7 +2,6 @@ export { default as Accordion } from "./Accordion/Accordion";
 export { default as Button } from "./Button/Button";
 export { default as Drawer } from "./Drawer/Drawer";
 export { default as Icon } from "./Icon/Icon";
-export { default as Image } from "./Image/Image";
 export { default as Menu } from "./Menu/Menu";
 export { default as Modal } from "./Modal/Modal";
 export { default as Slider } from "./Slider/Slider";
@@ -11,7 +10,6 @@ export type { Props as AccordionProps } from "./Accordion/Accordion";
 export type { Props as ButtonProps } from "./Button/Button";
 export type { Props as DrawerProps } from "./Drawer/Drawer";
 export type { Props as IconProps } from "./Icon/Icon";
-export type { Props as ImageProps } from "./Image/Image";
 export type {
   Props as MenuProps,
   MenuItemRecord,
