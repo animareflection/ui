14bbd3d805dcf31a36fc43dcb7ffaa74e9520610
Brian Cooper <brian@brian-cooper.com>
2023-08-24 20:48:16 -0500

    chore: remove duplicate word in comment

--- src/lib/utils/sleep.ts
+++ src/lib/utils/sleep.ts
@@ -1,5 +1,5 @@
 /**
- * Simulate "sleep" by resolving a Promise that resolves after a given number of milliseconds.
+ * Simulate "sleep" by fulfilling a Promise that resolves after a given number of milliseconds.
  */
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
