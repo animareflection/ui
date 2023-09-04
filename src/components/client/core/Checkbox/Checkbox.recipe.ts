93df662b1a52cc248761d73b3ce96bf071550307
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-09-04 11:35:38 -0500

    feat(checkbox): add checkbox recipe

---
+++ src/components/client/core/Checkbox/Checkbox.recipe.ts
@@ -0,0 +1,86 @@
+import { defineSlotRecipe } from "@pandacss/dev";
+
+export const checkboxRecipe = defineSlotRecipe({
+  className: "checkbox",
+  description: "The styles for the Accordion component",
+  slots: ["root", "control", "label"],
+  base: {
+    root: {
+      alignItems: "center",
+      display: "flex",
+      gap: 3,
+    },
+    label: {
+      color: "fg.emphasized",
+      fontWeight: "medium",
+      textStyle: "md",
+    },
+    control: {
+      alignItems: "center",
+      borderColor: "border.emphasized",
+      borderWidth: "1px",
+      color: "accent.fg",
+      cursor: "pointer",
+      display: "flex",
+      width: 5,
+      height: 5,
+      borderRadius: "xs",
+      "& svg": {
+        width: 3.5,
+        height: 3.5,
+      },
+      justifyContent: "center",
+      transitionDuration: "normal",
+      transitionProperty: "border-color, background",
+      transitionTimingFunction: "default",
+      _hover: {
+        background: "bg.subtle",
+      },
+      _checked: {
+        background: "accent.default",
+        borderColor: "border.accent",
+        _hover: {
+          background: "accent.default",
+        },
+      },
+    },
+  },
+  variants: {
+    size: {
+      sm: {
+        root: {
+          gap: 2,
+        },
+        control: {
+          width: 4,
+          height: 4,
+          borderRadius: "xs",
+          "& svg": {
+            width: 3,
+            height: 3,
+          },
+        },
+        label: {
+          textStyle: "sm",
+        },
+      },
+      lg: {
+        root: {
+          gap: 4,
+        },
+        control: {
+          width: 6,
+          height: 6,
+          borderRadius: "md",
+          "& svg": {
+            width: 4,
+            height: 4,
+          },
+        },
+        label: {
+          textStyle: "lg",
+        },
+      },
+    },
+  },
+});
