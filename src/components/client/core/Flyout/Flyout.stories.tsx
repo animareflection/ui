92e73c9d137c92b1a873ee818804dfbe6c216a0c
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2023-09-08 14:38:11 -0500

    feat: add placement story initial

--- src/components/client/core/Flyout/Flyout.stories.tsx
+++ src/components/client/core/Flyout/Flyout.stories.tsx
@@ -1,16 +1,80 @@
 import { flyoutState } from "./Flyout.spec";
 import { Button, Flyout } from "components/client";
 import { Text } from "components/universal";
-import { Flex } from "generated/panda/jsx";
+import { Flex, VStack } from "generated/panda/jsx";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
 type Story = StoryObj<typeof Flyout>;
 
+type Placement =
+  | "left-start"
+  | "left-end"
+  | "right-start"
+  | "right-end"
+  | "top-start"
+  | "top-end"
+  | "bottom-start"
+  | "bottom-end";
+
+const testArray: Placement[] = [
+  "bottom-start",
+  "bottom-end",
+  "left-start",
+  "left-end",
+  "right-start",
+  "right-end",
+  "top-start",
+  "top-end",
+];
+
+export const PlacementTest = () => {
+  return (
+    <VStack
+      gap={4}
+      h="screen"
+      w="full"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {testArray.map((item) => (
+        <Flyout
+          key={item}
+          placement={item}
+          trigger={<Button>{item}</Button>}
+          title="Flyout Title"
+        >
+          <Text mt={2}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam.
+          </Text>
+        </Flyout>
+      ))}
+    </VStack>
+  );
+};
+
 export const Default: Story = {
   render: () => (
+    <Flyout trigger={<Button>Open Flyout</Button>} title="Flyout Title">
+      <Text mt={2}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam.
+      </Text>
+    </Flyout>
+  ),
+};
+
+export const Placement: Story = {
+  render: () => (
     <Flex h="screen" w="full" justifyContent="center" alignItems="center">
-      <Flyout trigger={<Button>Open Flyout</Button>} title="Flyout Title">
+      <Flyout
+        placement="left-start"
+        trigger={<Button>Open Flyout</Button>}
+        title="Flyout Title"
+      >
         <Text mt={2}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
