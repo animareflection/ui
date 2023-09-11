3451f138bcae61e47666ffd8624397409c8720fd
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-09-11 17:34:09 -0500

    chore(toast): add min width to RHT toasts

--- src/components/client/core/Toaster/Toaster.tsx
+++ src/components/client/core/Toaster/Toaster.tsx
@@ -17,6 +17,7 @@ const Toaster = ({ ...props }: Props) => (
         borderWidth: "1px",
         borderColor: "var(--colors-border-default)",
         boxShadow: "var(--shadows-lg)",
+        minWidth: "var(--sizes-32)",
       },
       success: {
         style: {
