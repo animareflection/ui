0448475212dd6215f1080d6d74763e354bfdb753
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2023-09-13 10:52:43 -0500

    chore: remove react-router-dom in favor of demo pathname

--- examples/vite-react/src/App.tsx
+++ examples/vite-react/src/App.tsx
@@ -1,5 +1,3 @@
-import { BrowserRouter } from "react-router-dom";
-
 import {
   Circle,
   Flex,
@@ -39,103 +37,101 @@ import {
 import "main.css";
 
 const App = () => (
-  <BrowserRouter>
-    <Flex direction="column" h="100%" align="center" gap={4} py={12}>
-      <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
-        Anima Reflection UI library demo
-      </Text>
+  <Flex direction="column" h="100%" align="center" gap={4} py={12}>
+    <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
+      Anima Reflection UI library demo
+    </Text>
 
-      <Flex gap={4}>
-        <a href="https://vitejs.dev/" target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg"
-            width="50px"
-          />
-        </a>
+    <Flex gap={4}>
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener noreferrer">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg"
+          width="50px"
+        />
+      </a>
 
-        <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-            width="50px"
-          />
-        </a>
-      </Flex>
-
-      <Grid
-        gridTemplateColumns="1fr 1fr"
-        px={4}
-        w="100%"
-        alignItems="center"
-        justifyItems="center"
-      >
-        <Square
-          size={40}
-          color="white"
-          bgColor="brand.primary.500"
-          fontWeight="bold"
-          fontSize="2xl"
-        >
-          Square
-        </Square>
-
-        <Circle
-          size={40}
-          bgColor="brand.secondary.300"
-          fontWeight="bold"
-          fontSize="2xl"
-        >
-          Circle
-        </Circle>
-      </Grid>
-
-      <Flex direction="column" align="center" w="100%" gap={2} p={12}>
-        <ButtonDemo />
-        <AccordionDemo />
-        <DrawerDemo />
-        <IconDemo />
-        <MenuDemo />
-        <ModalDemo />
-        <SliderDemo />
-        <BadgeDemo />
-        <CardDemo />
-        <ImageDemo />
-        <SpinnerDemo />
-        <TabsDemo />
-        <ToggleDemo />
-        <SkeletonToggleDemo />
-        <CheckboxDemo />
-        <KbdDemo />
-        <CollapseDemo />
-        <BannerDemo />
-        <InputDemo />
-        <NumberInputDemo />
-        <FlyoutDemo />
-        <TooltipDemo />
-        <BreadcrumbDemo />
-
-        <Hide below="md">
-          <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
-            Hide below md breakpoint
-          </Text>
-        </Hide>
-        <Hide from="md">
-          <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
-            Hide from md breakpoint and up
-          </Text>
-        </Hide>
-        <Show below="md">
-          <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
-            Show below md breakpoint
-          </Text>
-        </Show>
-        <Show from="md">
-          <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
-            Show from md breakpoint and up
-          </Text>
-        </Show>
-      </Flex>
+      <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+          width="50px"
+        />
+      </a>
     </Flex>
-  </BrowserRouter>
+
+    <Grid
+      gridTemplateColumns="1fr 1fr"
+      px={4}
+      w="100%"
+      alignItems="center"
+      justifyItems="center"
+    >
+      <Square
+        size={40}
+        color="white"
+        bgColor="brand.primary.500"
+        fontWeight="bold"
+        fontSize="2xl"
+      >
+        Square
+      </Square>
+
+      <Circle
+        size={40}
+        bgColor="brand.secondary.300"
+        fontWeight="bold"
+        fontSize="2xl"
+      >
+        Circle
+      </Circle>
+    </Grid>
+
+    <Flex direction="column" align="center" w="100%" gap={2} p={12}>
+      <ButtonDemo />
+      <AccordionDemo />
+      <DrawerDemo />
+      <IconDemo />
+      <MenuDemo />
+      <ModalDemo />
+      <SliderDemo />
+      <BadgeDemo />
+      <CardDemo />
+      <ImageDemo />
+      <SpinnerDemo />
+      <TabsDemo />
+      <ToggleDemo />
+      <SkeletonToggleDemo />
+      <CheckboxDemo />
+      <KbdDemo />
+      <CollapseDemo />
+      <BannerDemo />
+      <InputDemo />
+      <NumberInputDemo />
+      <FlyoutDemo />
+      <TooltipDemo />
+      <BreadcrumbDemo />
+
+      <Hide below="md">
+        <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
+          Hide below md breakpoint
+        </Text>
+      </Hide>
+      <Hide from="md">
+        <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
+          Hide from md breakpoint and up
+        </Text>
+      </Hide>
+      <Show below="md">
+        <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
+          Show below md breakpoint
+        </Text>
+      </Show>
+      <Show from="md">
+        <Text color="brand.primary.500" fontSize="3xl" fontWeight="bold">
+          Show from md breakpoint and up
+        </Text>
+      </Show>
+    </Flex>
+  </Flex>
 );
 
 export default App;
--- examples/vite-react/src/components/BreadcrumbDemo.tsx
+++ examples/vite-react/src/components/BreadcrumbDemo.tsx
@@ -1,15 +1,17 @@
-import { useLocation } from "react-router-dom";
 import { Breadcrumb, Icon } from "@animareflection/ui/client";
 import { FiChevronRight as ChevronRightIcon } from "react-icons/fi";
 
 import { Wrapper } from "components";
 
 const BreadcrumbDemo = () => {
-  const location = useLocation();
+  // Note: To obtain the pathname in vite-react, use `useLocation()` from "react-router-dom".
+  // Ensure the App is wrapped in `BrowserRouter`.
+
+  const demoPathname = "breadcrumb";
   return (
     <Wrapper title="Breadcrumb">
       <Breadcrumb
-        pathname={location.pathname}
+        pathname={demoPathname}
         baseUrl="storybook.animareflection.com"
         SeparatorIcon={<Icon as={ChevronRightIcon} color="accent.emphasized" />}
       />
