f6a6e26dc8df1c50628bfea584be721289ca0ca0
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2023-09-26 16:46:34 -0500

    feat: add spec file and update storybook with toggle example

--- src/components/client/core/Avatar/Avatar.stories.tsx
+++ src/components/client/core/Avatar/Avatar.stories.tsx
@@ -1,23 +1,45 @@
-import { Avatar } from "components/client";
+import { useState } from "react";
+
+import { avatarState } from "./Avatar.spec";
+import { Avatar, Button } from "components/client";
+import { Flex } from "generated/panda/jsx";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
 type Story = StoryObj<typeof Avatar>;
 
-export const Default: Story = {
-  render: () => <Avatar src="/img/logo.png" alt="avatar" />,
+const AvatarToggle = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  return (
+    <Flex direction="column" gap={2}>
+      {isLoaded ? (
+        <Avatar src="/img/logo.png" alt="avatar" />
+      ) : (
+        <Avatar src="" alt="" />
+      )}
+      <Button
+        data-testid="toggle"
+        variant="ghost"
+        p={2}
+        onClick={() => setIsLoaded(!isLoaded)}
+      >
+        {isLoaded ? "Toggle to fallback" : "Toggle to image"}
+      </Button>
+    </Flex>
+  );
 };
 
-export const Fallback: Story = {
-  render: () => <Avatar src="" alt="" />,
+export const Toggle: Story = {
+  render: () => <AvatarToggle />,
 };
 
-//   export const AccordionState: Story = {
-//     ...Default,
-//     play: accordionState,
-//     name: "[TEST] Accordion State",
-//     tags: ["test"],
-//   };
+export const AvatarState: Story = {
+  ...Toggle,
+  play: avatarState,
+  name: "[TEST] Avatar State",
+  tags: ["test"],
+};
 
 // TODO remove explicit type annotation, required due to `pnpm` bug (and therefore Yarn with `pnpm` linker); https://github.com/microsoft/TypeScript/issues/47663
 const meta: Meta<typeof Avatar> = {
