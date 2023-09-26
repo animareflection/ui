d9a5b4adb5df9fc0778159f3f86745baa17ca98b
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-09-26 18:49:38 -0500

    fix: fix button TS issues, fix 'NumberInput' tests

--- src/components/core/Button/Button.tsx
+++ src/components/core/Button/Button.tsx
@@ -1,27 +1,19 @@
-import { ark } from "@ark-ui/react";
-
 import { panda } from "generated/panda/jsx";
 import { button } from "generated/panda/recipes";
 
 import type { ButtonVariantProps } from "generated/panda/recipes";
-import type { PandaComponent } from "generated/panda/types/jsx";
 import type { ComponentProps } from "react";
 
-export type Props = ComponentProps<typeof PandaButton> & ButtonVariantProps;
-
-const PandaButton: PandaComponent<typeof ark.button> = panda(
-  ark.button,
-  button,
-);
+export type Props = ComponentProps<typeof panda.button> & ButtonVariantProps;
 
 /**
  * Core UI button.
  */
 const Button = ({ variant, size, children, ...rest }: Props) => {
   return (
-    <PandaButton className={button({ variant, size })} {...rest}>
+    <panda.button className={button({ variant, size })} {...rest}>
       {children}
-    </PandaButton>
+    </panda.button>
   );
 };
 
