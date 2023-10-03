572766a1ad51d806a42a0af74fd00d2981be6b2e
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-10-03 16:48:15 -0500

    Fix: Update Basic Recipe Component Structures (#144)
    * fix: update button component structure
    
    * fix: update badge component structure
    
    * fix: update banner component structure
    
    * fix: update icon component structure
    
    * fix: add aria-label to banner close button
    
    * fix: update image component structure
    
    * fix: update spinner component structure
    
    * fix: update text component structure
    
    * fix: update hide and show component structures
    
    * ci(changesets): add changesets for component patches

---
+++ .changeset/hot-candles-dance.md
@@ -0,0 +1,5 @@
+---
+"@animareflection/ui": patch
+---
+
+Fix `className` omission issue for basic recipe components when used as child for `asChild` components
--- src/components/core/Text/Text.tsx
+++ src/components/core/Text/Text.tsx
@@ -1,20 +1,13 @@
 import { panda } from "generated/panda/jsx";
 import { text } from "generated/panda/recipes";
 
-import type { TextVariantProps } from "generated/panda/recipes";
 import type { ComponentProps } from "react";
 
-export type Props = ComponentProps<typeof PandaText> & TextVariantProps;
-
-const PandaText = panda("p", text);
+export type Props = ComponentProps<typeof Text>;
 
 /**
  * Core UI text (typography).
  */
-const Text = ({ size, children, ...rest }: Props) => (
-  <PandaText className={text({ size })} {...rest}>
-    {children}
-  </PandaText>
-);
+const Text = panda("p", text);
 
 export default Text;
