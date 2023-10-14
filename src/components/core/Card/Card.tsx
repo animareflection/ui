971fe99a742eefc7feac7b50f2fb605c73a649b7
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-10-14 11:19:28 -0500

    chore(card): remove displayName check for card component children

--- src/components/core/Card/Card.tsx
+++ src/components/core/Card/Card.tsx
@@ -1,6 +1,5 @@
 import { panda } from "generated/panda/jsx";
 import { card } from "generated/panda/recipes";
-import { getChildrenOnDisplayName } from "lib/utils";
 
 import type { HTMLPandaProps } from "generated/panda/jsx";
 
@@ -11,28 +10,11 @@ type Props = HTMLPandaProps<"div">;
 /**
  * Core UI card.
  */
-const Card = ({ children, ...rest }: Props) => {
-  const cardHeader = getChildrenOnDisplayName({
-    children,
-    displayName: "CardHeader",
-  });
-  const cardBody = getChildrenOnDisplayName({
-    children,
-    displayName: "CardBody",
-  });
-  const cardFooter = getChildrenOnDisplayName({
-    children,
-    displayName: "CardFooter",
-  });
-
-  return (
-    <panda.div className={cardClasses.root} {...rest}>
-      {cardHeader}
-      {cardBody}
-      {cardFooter}
-    </panda.div>
-  );
-};
+const Card = ({ children, ...rest }: Props) => (
+  <panda.div className={cardClasses.root} {...rest}>
+    {children}
+  </panda.div>
+);
 
 export const CardHeader = ({ children, ...rest }: Props) => (
   <panda.div className={cardClasses.header} {...rest}>
@@ -40,23 +22,17 @@ export const CardHeader = ({ children, ...rest }: Props) => (
   </panda.div>
 );
 
-CardHeader.displayName = "CardHeader";
-
 export const CardBody = ({ children, ...rest }: Props) => (
   <panda.div className={cardClasses.body} {...rest}>
     {children}
   </panda.div>
 );
 
-CardBody.displayName = "CardBody";
-
 export const CardFooter = ({ children, ...rest }: Props) => (
   <panda.div className={cardClasses.footer} {...rest}>
     {children}
   </panda.div>
 );
-
-CardFooter.displayName = "CardFooter";
 
 export type {
   Props as CardProps,
