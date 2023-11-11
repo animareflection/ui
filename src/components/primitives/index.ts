ba227b5ae47d9fa5dcf7d6a747449ae66dfc0918
Brian Cooper <brian@brian-cooper.com>
2023-11-10 22:36:08 -0600

    refactor: simplify primitives index file

--- src/components/primitives/index.ts
+++ src/components/primitives/index.ts
@@ -1,239 +1,41 @@
-export {
-  default as PrimitiveAccordion,
-  PrimitiveAccordionItem,
-  PrimitiveAccordionItemTrigger,
-  PrimitiveAccordionItemContent,
-} from "./Accordion/Accordion";
-export {
-  default as PrimitiveAvatar,
-  PrimitiveAvatarFallback,
-  PrimitiveAvatarImage,
-} from "./Avatar/Avatar";
-export {
-  default as PrimitiveCarousel,
-  PrimitiveCarouselControl,
-  PrimitiveCarouselNextTrigger,
-  PrimitiveCarouselPrevTrigger,
-  PrimitiveCarouselItem,
-  PrimitiveCarouselItemGroup,
-  PrimitiveCarouselViewport,
-  PrimitiveCarouselIndicator,
-  PrimitiveCarouselIndicatorGroup,
-} from "./Carousel/Carousel";
-export {
-  default as PrimitiveCheckbox,
-  PrimitiveCheckboxControl,
-  PrimitiveCheckboxLabel,
-} from "./Checkbox/Checkbox";
-export {
-  default as PrimitiveDrawer,
-  PrimitiveDrawerBackdrop,
-  PrimitiveDrawerTrigger,
-  PrimitiveDrawerPositioner,
-  PrimitiveDrawerContent,
-  PrimitiveDrawerCloseTrigger,
-  PrimitiveDrawerTitle,
-  PrimitiveDrawerDescription,
-} from "./Drawer/Drawer";
-export {
-  default as PrimitiveMenu,
-  PrimitiveMenuArrow,
-  PrimitiveMenuArrowTip,
-  PrimitiveMenuContent,
-  PrimitiveMenuContextTrigger,
-  PrimitiveMenuItem,
-  PrimitiveMenuItemGroup,
-  PrimitiveMenuItemGroupLabel,
-  PrimitiveMenuOptionItem,
-  PrimitiveMenuPositioner,
-  PrimitiveMenuSeparator,
-  PrimitiveMenuTrigger,
-  PrimitiveMenuTriggerItem,
-} from "./Menu/Menu";
-export {
-  default as PrimitiveModal,
-  PrimitiveModalBackdrop,
-  PrimitiveModalTrigger,
-  PrimitiveModalPositioner,
-  PrimitiveModalContent,
-  PrimitiveModalCloseTrigger,
-  PrimitiveModalTitle,
-  PrimitiveModalDescription,
-} from "./Modal/Modal";
-export {
-  default as PrimitiveNumberInput,
-  PrimitiveNumberInputControl,
-  PrimitiveNumberInputDecrementTrigger,
-  PrimitiveNumberInputIncrementTrigger,
-  PrimitiveNumberInputInput,
-  PrimitiveNumberInputLabel,
-  PrimitiveNumberInputScrubber,
-} from "./NumberInput/NumberInput";
-export {
-  default as PrimitiveRadioGroup,
-  PrimitiveRadioGroupItemControl,
-  PrimitiveRadioGroupItem,
-  PrimitiveRadioGroupLabel,
-} from "./RadioGroup/RadioGroup";
-export {
-  default as PrimitiveFlyout,
-  PrimitiveFlyoutArrow,
-  PrimitiveFlyoutArrowTip,
-  PrimitiveFlyoutCloseTrigger,
-  PrimitiveFlyoutContent,
-  PrimitiveFlyoutDescription,
-  PrimitiveFlyoutPositioner,
-  PrimitiveFlyoutTitle,
-  PrimitiveFlyoutTrigger,
-} from "./Flyout/Flyout";
-export {
-  default as PrimitiveSlider,
-  PrimitiveSliderControl,
-  PrimitiveSliderLabel,
-  PrimitiveSliderMarker,
-  PrimitiveSliderMarkerGroup,
-  PrimitiveSliderRange,
-  PrimitiveSliderThumb,
-  PrimitiveSliderTrack,
-} from "./Slider/Slider";
-export {
-  default as PrimitiveTabs,
-  PrimitiveTabContent,
-  PrimitiveTabIndicator,
-  PrimitiveTabList,
-  PrimitiveTabTrigger,
-} from "./Tabs/Tabs";
-export {
-  default as PrimitiveToggle,
-  PrimitiveToggleControl,
-  PrimitiveToggleLabel,
-  PrimitiveToggleThumb,
-} from "./Toggle/Toggle";
-export {
-  default as PrimitiveTooltip,
-  PrimitiveTooltipArrow,
-  PrimitiveTooltipArrowTip,
-  PrimitiveTooltipContent,
-  PrimitiveTooltipPositioner,
-  PrimitiveTooltipTrigger,
-} from "./Tooltip/Tooltip";
+export { default as PrimitiveAccordion } from "./Accordion/Accordion";
+export * from "./Accordion/Accordion";
 
-export type {
-  PrimitiveAccordionProps,
-  PrimitiveAccordionItemProps,
-  PrimitiveAccordionItemTriggerProps,
-  PrimitiveAccordionItemContentProps,
-} from "./Accordion/Accordion";
-export type {
-  PrimitiveAvatarProps,
-  PrimitiveAvatarFallbackProps,
-  PrimitiveAvatarImageProps,
-} from "./Avatar/Avatar";
-export type {
-  PrimitiveCarouselProps,
-  PrimitiveCarouselControlProps,
-  PrimitiveCarouselNextTriggerProps,
-  PrimitiveCarouselPrevTriggerProps,
-  PrimitiveCarouselItemProps,
-  PrimitiveCarouselItemGroupProps,
-  PrimitiveCarouselViewportProps,
-  PrimitiveCarouselIndicatorProps,
-  PrimitiveCarouselIndicatorGroupProps,
-} from "./Carousel/Carousel";
-export type {
-  PrimitiveCheckboxProps,
-  PrimitiveCheckboxControlProps,
-  PrimitiveCheckboxLabelProps,
-} from "./Checkbox/Checkbox";
-export type {
-  PrimitiveDrawerProps,
-  PrimitiveDrawerBackdropProps,
-  PrimitiveDrawerCloseTriggerProps,
-  PrimitiveDrawerPositionerProps,
-  PrimitiveDrawerContentProps,
-  PrimitiveDrawerDescriptionProps,
-  PrimitiveDrawerTitleProps,
-  PrimitiveDrawerTriggerProps,
-} from "./Drawer/Drawer";
-export type {
-  PrimitiveMenuProps,
-  PrimitiveMenuArrowProps,
-  PrimitiveMenuArrowTipProps,
-  PrimitiveMenuContentProps,
-  PrimitiveMenuContextTriggerProps,
-  PrimitiveMenuItemProps,
-  PrimitiveMenuItemGroupProps,
-  PrimitiveMenuItemGroupLabelProps,
-  PrimitiveMenuOptionItemProps,
-  PrimitiveMenuPositionerProps,
-  PrimitiveMenuSeparatorProps,
-  PrimitiveMenuTriggerProps,
-  PrimitiveMenuTriggerItemProps,
-} from "./Menu/Menu";
-export type {
-  PrimitiveModalProps,
-  PrimitiveModalBackdropProps,
-  PrimitiveModalCloseTriggerProps,
-  PrimitiveModalPositionerProps,
-  PrimitiveModalContentProps,
-  PrimitiveModalDescriptionProps,
-  PrimitiveModalTitleProps,
-  PrimitiveModalTriggerProps,
-} from "./Modal/Modal";
-export type {
-  PrimitiveNumberInputControlProps,
-  PrimitiveNumberInputDecrementTriggerProps,
-  PrimitiveNumberInputIncrementTriggerProps,
-  PrimitiveNumberInputInputProps,
-  PrimitiveNumberInputLabelProps,
-  PrimitiveNumberInputProps,
-  PrimitiveNumberInputScrubberProps,
-} from "./NumberInput/NumberInput";
-export type {
-  PrimitiveRadioGroupItemControlProps,
-  PrimitiveRadioGroupLabelProps,
-  PrimitiveRadioGroupItemProps,
-  PrimitiveRadioGroupProps,
-} from "./RadioGroup/RadioGroup";
-export type {
-  PrimitiveFlyoutProps,
-  PrimitiveFlyoutArrowProps,
-  PrimitiveFlyoutArrowTipProps,
-  PrimitiveFlyoutCloseTriggerProps,
-  PrimitiveFlyoutContentProps,
-  PrimitiveFlyoutDescriptionProps,
-  PrimitiveFlyoutPositionerProps,
-  PrimitiveFlyoutTitleProps,
-  PrimitiveFlyoutTriggerProps,
-} from "./Flyout/Flyout";
-export type {
-  PrimitiveSliderProps,
-  PrimitiveSliderControlProps,
-  PrimitiveSliderLabelProps,
-  PrimitiveSliderMarkerProps,
-  PrimitiveSliderMarkerGroupProps,
-  PrimitiveSliderRangeProps,
-  PrimitiveSliderThumbProps,
-  PrimitiveSliderTrackProps,
-} from "./Slider/Slider";
-export type {
-  PrimitiveTabsProps,
-  PrimitiveTabContentProps,
-  PrimitiveTabIndicatorProps,
-  PrimitiveTabListProps,
-  PrimitiveTabTriggerProps,
-} from "./Tabs/Tabs";
-export type {
-  PrimitiveToggleProps,
-  PrimitiveToggleControlProps,
-  PrimitiveToggleLabelProps,
-  PrimitiveToggleThumbProps,
-} from "./Toggle/Toggle";
-export type {
-  PrimitiveTooltipProps,
-  PrimitiveTooltipArrowProps,
-  PrimitiveTooltipArrowTipProps,
-  PrimitiveTooltipContentProps,
-  PrimitiveTooltipPositionerProps,
-  PrimitiveTooltipTriggerProps,
-} from "./Tooltip/Tooltip";
+export { default as PrimitiveAvatar } from "./Avatar/Avatar";
+export * from "./Avatar/Avatar";
+
+export { default as PrimitiveCarousel } from "./Carousel/Carousel";
+export * from "./Carousel/Carousel";
+
+export { default as PrimitiveCheckbox } from "./Checkbox/Checkbox";
+export * from "./Checkbox/Checkbox";
+
+export { default as PrimitiveDrawer } from "./Drawer/Drawer";
+export * from "./Drawer/Drawer";
+
+export { default as PrimitiveMenu } from "./Menu/Menu";
+export * from "./Menu/Menu";
+
+export { default as PrimitiveModal } from "./Modal/Modal";
+export * from "./Modal/Modal";
+
+export { default as PrimitiveNumberInput } from "./NumberInput/NumberInput";
+export * from "./NumberInput/NumberInput";
+
+export { default as PrimitiveRadioGroup } from "./RadioGroup/RadioGroup";
+export * from "./RadioGroup/RadioGroup";
+
+export { default as PrimitiveFlyout } from "./Flyout/Flyout";
+export * from "./Flyout/Flyout";
+
+export { default as PrimitiveSlider } from "./Slider/Slider";
+export * from "./Slider/Slider";
+
+export { default as PrimitiveTabs } from "./Tabs/Tabs";
+export * from "./Tabs/Tabs";
+
+export { default as PrimitiveToggle } from "./Toggle/Toggle";
+export * from "./Toggle/Toggle";
+
+export { default as PrimitiveTooltip } from "./Tooltip/Tooltip";
+export * from "./Tooltip/Tooltip";
