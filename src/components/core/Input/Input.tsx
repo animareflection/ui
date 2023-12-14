84dc5dbd92c18c0fbeb0bc40bfa52b6dcd0b2312
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2023-12-13 18:31:17 -0600

    refactor: include forwardRef for the entire component and attach the ref to the input element

--- src/components/core/Input/Input.tsx
+++ src/components/core/Input/Input.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/display-name */
 import { ark } from "@ark-ui/react";
+import { forwardRef } from "react";
 
 import { Flex, panda, Stack } from "generated/panda/jsx";
 import { input } from "generated/panda/recipes";
 
 import type { StackProps } from "generated/panda/jsx";
 import type { InputVariantProps } from "generated/panda/recipes";
-import type { ComponentProps, ReactNode } from "react";
+import type { ComponentProps, ReactNode, Ref } from "react";
 
 export interface Props
   extends ComponentProps<typeof PandaInput>,
@@ -24,59 +26,65 @@ const PandaLabel = panda(ark.label);
 /**
  * Core UI input.
  */
-const Input = ({
-  label,
-  leftAddon,
-  rightAddon,
-  inputLeftElement,
-  inputRightElement,
-  variant,
-  size,
-  containerProps,
-  ...rest
-}: Props) => {
-  const classNames = input({ size, variant });
+const Input = forwardRef(
+  (
+    {
+      label,
+      leftAddon,
+      rightAddon,
+      inputLeftElement,
+      inputRightElement,
+      variant,
+      size,
+      containerProps,
+      ...rest
+    }: Props,
+    ref: Ref<HTMLInputElement> | undefined,
+  ) => {
+    const classNames = input({ size, variant });
 
-  return (
-    <Stack gap={1.5} {...containerProps}>
-      {label && <PandaLabel className={classNames.label}>{label}</PandaLabel>}
-      <Flex>
-        {leftAddon && (
-          <panda.div className={classNames.addon} borderLeftRadius="sm">
-            {leftAddon}
-          </panda.div>
-        )}
-        <Flex pos="relative" w="100%">
-          {inputLeftElement && (
-            <panda.div className={classNames.leftElement}>
-              {inputLeftElement}
+    return (
+      <Stack gap={1.5} {...containerProps}>
+        {label && <PandaLabel className={classNames.label}>{label}</PandaLabel>}
+        <Flex>
+          {leftAddon && (
+            <panda.div className={classNames.addon} borderLeftRadius="sm">
+              {leftAddon}
             </panda.div>
           )}
-          <PandaInput
-            className={classNames.input}
-            borderTopLeftRadius={leftAddon ? 0 : "sm"}
-            borderBottomLeftRadius={leftAddon ? 0 : "sm"}
-            borderTopRightRadius={rightAddon ? 0 : "sm"}
-            borderBottomRightRadius={rightAddon ? 0 : "sm"}
-            pl={inputLeftElement ? 10 : 3}
-            pr={inputRightElement ? 10 : 3}
-            {...rest}
-          />
-          {inputRightElement && (
-            <panda.div className={classNames.rightElement}>
-              {inputRightElement}
+          <Flex pos="relative" w="100%">
+            {inputLeftElement && (
+              <panda.div className={classNames.leftElement}>
+                {inputLeftElement}
+              </panda.div>
+            )}
+            <PandaInput
+              ref={ref}
+              className={classNames.input}
+              borderTopLeftRadius={leftAddon ? 0 : "sm"}
+              borderBottomLeftRadius={leftAddon ? 0 : "sm"}
+              borderTopRightRadius={rightAddon ? 0 : "sm"}
+              borderBottomRightRadius={rightAddon ? 0 : "sm"}
+              pl={inputLeftElement ? 10 : 3}
+              pr={inputRightElement ? 10 : 3}
+              {...rest}
+            />
+            {inputRightElement && (
+              <panda.div className={classNames.rightElement}>
+                {inputRightElement}
+              </panda.div>
+            )}
+          </Flex>
+
+          {rightAddon && (
+            <panda.div className={classNames.addon} borderRightRadius="sm">
+              {rightAddon}
             </panda.div>
           )}
         </Flex>
-
-        {rightAddon && (
-          <panda.div className={classNames.addon} borderRightRadius="sm">
-            {rightAddon}
-          </panda.div>
-        )}
-      </Flex>
-    </Stack>
-  );
-};
+      </Stack>
+    );
+  },
+);
 
 export default Input;
