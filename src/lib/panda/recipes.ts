7f610afe494a369bb9de604b53d2ccaf2b517102
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-12-27 23:03:01 -0600

    feat(combobox): add base for recipe

--- src/lib/panda/recipes.ts
+++ src/lib/panda/recipes.ts
@@ -8,6 +8,7 @@ import { buttonRecipe } from "../../components/core/Button/Button.recipe";
 import { cardRecipe } from "../../components/core/Card/Card.recipe";
 import { carouselRecipe } from "../../components/core/Carousel/Carousel.recipe";
 import { checkboxRecipe } from "../../components/core/Checkbox/Checkbox.recipe";
+import { comboboxRecipe } from "../../components/core/Combobox/Combobox.recipe";
 import { drawerRecipe } from "../../components/core/Drawer/Drawer.recipe";
 import { flyoutRecipe } from "../../components/core/Flyout/Flyout.recipe";
 import { iconRecipe } from "../../components/core/Icon/Icon.recipe";
@@ -46,13 +47,14 @@ export const slotRecipes = {
   accordion: accordionRecipe,
   alert: alertRecipe,
   avatar: avatarRecipe,
-  carousel: carouselRecipe,
   breadcrumb: breadcrumbRecipe,
-  checkbox: checkboxRecipe,
   card: cardRecipe,
+  carousel: carouselRecipe,
+  checkbox: checkboxRecipe,
+  combobox: comboboxRecipe,
   drawer: drawerRecipe,
-  input: inputRecipe,
   flyout: flyoutRecipe,
+  input: inputRecipe,
   menu: menuRecipe,
   modal: modalRecipe,
   numberInput: numberInputRecipe,
