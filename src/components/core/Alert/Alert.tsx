1bcd17e79e0eb0b0114f0a013e7d82b2b46bd095
Brian Cooper <brian@brian-cooper.com>
2023-12-29 03:40:27 -0600

    refactor(alert): apply style context provider

--- src/components/core/Alert/Alert.tsx
+++ src/components/core/Alert/Alert.tsx
@@ -3,10 +3,23 @@ import { FiInfo } from "react-icons/fi";
 import Icon from "components/core/Icon/Icon";
 import Text from "components/core/Text/Text";
 import { Flex } from "generated/panda/jsx";
+import { alert, type AlertVariantProps } from "generated/panda/recipes";
+import { createStyleContext } from "lib/util";
 
 import type { FlexProps } from "generated/panda/jsx";
-import type { AlertVariantProps } from "generated/panda/recipes";
 import type { ReactElement } from "react";
+
+const { withProvider, withContext } = createStyleContext(alert);
+
+// TODO extract primitives, add prop interfaces, and export both
+
+const AlertRoot = withProvider(Flex, "root");
+
+const AlertIcon = withContext(Icon, "icon");
+
+const AlertTitle = withContext(Text, "title");
+
+const AlertDescription = withContext(Text, "description");
 
 export interface Props extends FlexProps, AlertVariantProps {
   icon?: ReactElement;
@@ -15,13 +28,15 @@ export interface Props extends FlexProps, AlertVariantProps {
 }
 
 const Alert = ({ icon, title, description, ...rest }: Props) => (
-  <Flex {...rest}>
-    <Icon>{icon ?? <FiInfo />}</Icon>
+  <AlertRoot {...rest}>
+    <AlertIcon>{icon ?? <FiInfo />}</AlertIcon>
+
     <Flex direction="column" gap={1}>
-      <Text>{title}</Text>
-      {description && <Text>{description}</Text>}
+      <AlertTitle>{title}</AlertTitle>
+
+      {description && <AlertDescription>{description}</AlertDescription>}
     </Flex>
-  </Flex>
+  </AlertRoot>
 );
 
 export default Alert;
