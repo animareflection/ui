e50581d0885e125f1ffe7c23bfd9dacd43c46368
Brian Cooper <brian@brian-cooper.com>
2023-12-28 19:51:18 -0600

    fix(date-picker): fix default story, add range input story

--- src/components/core/DatePicker/DatePicker.stories.tsx
+++ src/components/core/DatePicker/DatePicker.stories.tsx
@@ -4,39 +4,48 @@ import { datePickerState } from "./DatePicker.spec";
 import { DatePicker, Input } from "components/core";
 import Button from "components/core/Button/Button";
 import Icon from "components/core/Icon/Icon";
+import { input } from "generated/panda/recipes";
 
 import type { Meta, StoryObj } from "@storybook/react";
+import type { DatePickerProps } from "components/core";
 
 type Story = StoryObj<typeof DatePicker>;
 
+const DatePickerTemplate = (props: Partial<DatePickerProps>) => (
+  <DatePicker
+    label="Date Picker"
+    startOfWeek={1}
+    clearTrigger={
+      <Button size="xs" variant="ghost">
+        Clear
+      </Button>
+    }
+    trigger={
+      <Button size="xs">
+        <Icon color="bg.default" h={6} w={6}>
+          <CalendarIcon />
+        </Icon>
+      </Button>
+    }
+    input={
+      <Input
+        // TODO remove
+        className={input().input}
+        h={10}
+        placeholder="mm/dd/yyyy"
+      />
+    }
+    {...props}
+  />
+);
+
 export const Default: Story = {
+  render: DatePickerTemplate,
+};
+
+export const Range: Story = {
   render: () => (
-    <DatePicker
-      clearTrigger
-      input={
-        <Input
-          color="fg.default"
-          _focus={{
-            borderColor: "border.accent",
-            boxShadow: "sm",
-          }}
-          outline={0}
-          backgroundColor="bg.default"
-          borderWidth="1px"
-          borderColor="border.default"
-          h={10}
-          mt={-1.5}
-          placeholder="mm/dd/yyyy"
-        />
-      }
-      trigger={
-        <Button size="xs">
-          <Icon h={6} w={6}>
-            <CalendarIcon />
-          </Icon>
-        </Button>
-      }
-    />
+    <DatePickerTemplate selectionMode="range" label="Date Picker (Range)" />
   ),
 };
 
@@ -51,7 +60,6 @@ const meta = {
   title: "Components/Core/DatePicker",
   component: DatePicker,
   tags: ["autodocs"],
-  decorators: [(Story) => <Story />],
 } satisfies Meta<typeof DatePicker>;
 
 export default meta;
