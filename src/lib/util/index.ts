d0fac53341b44daa8099bd05a014a8bf844661bc
Brian Cooper <brian@brian-cooper.com>
2023-12-29 01:13:34 -0600

    refactor: 'utils' -> 'util'

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,3 +1,4 @@
+export { default as createStyleContext } from "./createStyleContext";
 export { default as emToPx } from "./emToPx";
 export { default as mapArrayToObject } from "./mapArrayToObject";
 export { default as sleep } from "./sleep";
