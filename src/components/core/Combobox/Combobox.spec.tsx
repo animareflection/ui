679a2baab9a082aac5c6597caf0ec853a14ab0b1
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2023-12-29 22:19:12 -0600

    test(combobox): update testing suite

--- src/components/core/Combobox/Combobox.spec.tsx
+++ src/components/core/Combobox/Combobox.spec.tsx
@@ -1,6 +1,8 @@
 import { expect } from "@storybook/jest";
 import { userEvent, within } from "@storybook/testing-library";
 
+import { sleep } from "lib/util";
+
 import type { ReactRenderer } from "@storybook/react";
 import type { PlayFunctionContext, Renderer } from "@storybook/types";
 
@@ -18,11 +20,14 @@ export const comboboxState = async <R extends Renderer = ReactRenderer>({
   const trigger = canvas.getByLabelText("open options");
 
   const bananaOption = canvas.getByText("Banana");
+  const vegetableGroup = canvas.getByText("Vegetables");
 
   await step("It should display options on trigger click", async () => {
     await expect(trigger).toBeVisible();
 
     await userEvent.click(trigger);
+
+    await sleep(100);
 
     await expect(canvas.getByText("Apple")).toBeVisible();
 
@@ -41,5 +46,6 @@ export const comboboxState = async <R extends Renderer = ReactRenderer>({
 
   await step("It should filter options on input change", async () => {
     await expect(bananaOption).not.toBeVisible();
+    await expect(vegetableGroup).not.toBeVisible();
   });
 };
