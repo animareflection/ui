5087e323cc421a980afd82830680d91d389f9102
Brian Cooper <brian@brian-cooper.com>
2023-12-30 19:59:01 -0600

    refactor(date-picker,examples): simplify demos

--- examples/vite-react/src/components/DatePickerDemo.tsx
+++ examples/vite-react/src/components/DatePickerDemo.tsx
@@ -14,40 +14,7 @@ import { Wrapper } from "components";
 
 const DatePickerDemo = () => (
   <Wrapper title="Date Picker">
-    <DatePicker
-      label="Date Picker"
-      startOfWeek={1}
-      clearTrigger={
-        <Flex
-          gap={1}
-          justify="center"
-          css={{
-            "& *": { color: "accent.default" },
-          }}
-        >
-          <Icon w={4}>
-            <FiX />
-          </Icon>
-
-          <Text fontSize="sm">Clear</Text>
-        </Flex>
-      }
-      trigger={
-        <Button size="xs">
-          <Icon color="bg.default" h={6} w={6}>
-            <CalendarIcon />
-          </Icon>
-        </Button>
-      }
-      input={
-        <Input
-          // TODO remove, this will be improved once slot recipe classes are automatically picked up
-          className={input().input}
-          h={10}
-          placeholder="mm/dd/yyyy"
-        />
-      }
-    />
+    <DatePicker label="Date Picker" startOfWeek={1} />
   </Wrapper>
 );
 
