187a21f1dade2b49c6d14fbbbb165681c55092d8
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-01-03 14:37:11 -0600

    fix: update descriptions for layer styles

--- src/lib/panda/layerStyles.ts
+++ src/lib/panda/layerStyles.ts
@@ -2,14 +2,14 @@ import { defineLayerStyles } from "@pandacss/dev";
 
 const layerStyles = defineLayerStyles({
   accentGradient: {
-    description: "layer styles for gradient",
+    description: "layer styles for accent gradient",
     value: {
       background:
         "linear-gradient(to right, token(colors.brand.secondary.500), token(colors.accent.default))",
     },
   },
   subtleGradient: {
-    description: "layer styles for gradient",
+    description: "layer styles for subtle gradient",
     value: {
       background: {
         base: "linear-gradient(to right, token(colors.brand.secondary.50), token(colors.brand.primary.50))",
