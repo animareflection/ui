04ca56ce857ea043858f102f24c9719f4f2b08dd
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-01-03 14:24:05 -0600

    chore: remove unnecessary button layer style from preset

--- src/lib/panda/layerStyles.ts
+++ src/lib/panda/layerStyles.ts
@@ -1,30 +1,6 @@
 import { defineLayerStyles } from "@pandacss/dev";
 
 const layerStyles = defineLayerStyles({
-  // TODO: remove button layer style when colorPalette composition strategy is implemented
-  // !NB: important flags are used to override recipe styles no matter what variant is used
-  button: {
-    description: "layer styles for brand.secondary button",
-    value: {
-      backgroundColor: {
-        base: "bg.default!",
-        _disabled: "bg.default!",
-        _hover: {
-          base: "brand.secondary.50!",
-          _dark: "brand.secondary.950!",
-          _disabled: "bg.default!",
-        },
-      },
-      borderColor: {
-        base: "brand.secondary.500!",
-        _disabled: { base: "border.disabled!", _hover: "border.disabled!" },
-      },
-      color: {
-        base: "brand.secondary.500!",
-        _disabled: { base: "fg.disabled!", _hover: "fg.disabled!" },
-      },
-    },
-  },
   gradient: {
     description: "layer styles for gradient",
     value: {
