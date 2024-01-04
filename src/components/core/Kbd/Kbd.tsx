5a951fba5cbf51a609562ba4d3d99e8286c919b3
Brian Cooper <brian@brian-cooper.com>
2024-01-03 21:25:57 -0600

    chore(kbd): remove explicit return

--- src/components/core/Kbd/Kbd.tsx
+++ src/components/core/Kbd/Kbd.tsx
@@ -9,24 +9,22 @@ export interface Props {
 /**
  * Core UI Keyboard component.
  */
-const Kbd = ({ children }: Props) => {
-  return (
-    <panda.kbd
-      display="inline-flex"
-      px={2}
-      py={1}
-      fontSize="xs"
-      fontFamily="code"
-      fontWeight="semibold"
-      color="neutral.950"
-      bgColor={{ base: "gray.100", _dark: "gray.400" }}
-      borderRadius="lg"
-      borderWidth="1px 1px 2px"
-      borderColor={{ base: "gray.200", _dark: "gray.500" }}
-    >
-      {children}
-    </panda.kbd>
-  );
-};
+const Kbd = ({ children }: Props) => (
+  <panda.kbd
+    display="inline-flex"
+    px={2}
+    py={1}
+    fontSize="xs"
+    fontFamily="code"
+    fontWeight="semibold"
+    color="neutral.950"
+    bgColor={{ base: "gray.100", _dark: "gray.400" }}
+    borderRadius="lg"
+    borderWidth="1px 1px 2px"
+    borderColor={{ base: "gray.200", _dark: "gray.500" }}
+  >
+    {children}
+  </panda.kbd>
+);
 
 export default Kbd;
