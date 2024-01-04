3b100b7b9a7cdb678fb314a9458acd4a26ed14e4
Brian Cooper <brian@brian-cooper.com>
2024-01-04 15:11:54 -0600

    refactor(next): migrate from deprecated image domains -> remote patterns

--- examples/next/next.config.js
+++ examples/next/next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["tinyurl.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tinyurl.com",
+      },
+    ],
   },
 };
 
