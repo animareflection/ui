58c8ca2cf8e55b19481bc56e64ef2d853fd46106
Brian Cooper <brian@brian-cooper.com>
2024-01-05 00:13:26 -0600

    chore(examples,next): use object-based PostCSS config

--- examples/next/postcss.config.js
+++ examples/next/postcss.config.js
@@ -1,23 +1,16 @@
 // based on Next.js PostCSS default: https://nextjs.org/docs/pages/building-your-application/configuring/post-css#customizing-plugins
 module.exports = {
-  plugins:
-    process.env.NODE_ENV === "production"
-      ? [
-          "@pandacss/dev/postcss",
-          "postcss-flexbugs-fixes",
-          [
-            "postcss-preset-env",
-            {
-              autoprefixer: {
-                flexbox: "no-2009",
-              },
-              stage: 3,
-              features: {
-                "custom-properties": false,
-              },
-            },
-          ],
-        ]
-      : // no transformations in development
-        [],
+  plugins: {
+    "@pandacss/dev/postcss": {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      stage: 3,
+      features: {
+        "custom-properties": false,
+      },
+    },
+  },
 };
